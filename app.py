a3951b270cbf5807918ccb7a7ccbf4fa10077f42
Abhijeet <abhijeet@5ire.org>
2024-09-17 13:39:16 +0530

    fix the port

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ app.add_middleware(
 )
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("app:app", host="localhost", port=8080, reload=True)
 
 
 @app.get("/get-courses")
